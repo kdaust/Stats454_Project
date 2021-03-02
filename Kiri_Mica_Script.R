e9cb3697133eb70e4bd41bba5b5f3067a7ae4487
Mica <granthag@ualberta.ca>
2021-03-01 18:35:58 -0800

    Update Kiri_Mica_Script.R

--- Kiri_Mica_Script.R
+++ Kiri_Mica_Script.R
@@ -350,3 +350,136 @@ boxplot(miscls, main = "Misclassification values for classes for LDA", names  = 
 boxplot(precision, main = "Precision values for classes for LDA", names = celltypenames)
 boxplot(recall, main = "Recall values for classes for LDA", names = celltypenames)
 
+##########################################################
+### KNN
+##########################################################
+              
+##Genetic Algorithm
+##fitness function
+calc_fitness <- function(vars, data_x, data_y){
+  cvFold <- createFolds(y = data_y, k = 3, list = F)
+  varNames <- colnames(data_x)
+  names_2 <- varNames[vars==1]
+  # get the columns of the current solution
+  data_sol=data_x[, names_2]
+  
+  acc <- numeric(length = 3L)
+  for(i in 1:3){
+    cat(".")
+    YTrain <- data_y[cvFold != i]
+    YTest <- data_y[cvFold == i]
+    XTrain <- data_sol[cvFold != i,]
+    XTest <- data_sol[cvFold == i,]
+    temp_dat <- data.frame(yy = YTrain, XTrain)
+    knn.fit1 <- knn(as.data.frame(XTrain), as.data.frame(XTest), YTrain, k=3)
+    knn.fit2 <- knn(as.data.frame(XTrain), as.data.frame(XTest), YTrain, k=5)
+    knn.fit3 <- knn(as.data.frame(XTrain), as.data.frame(XTest), YTrain, k=7)
+    
+    diff1 <- knn.fit1 == YTest
+    diff2 <- knn.fit2 == YTest
+    diff3 <- knn.fit3 == YTest
+    diff <- max(c(length(diff1), length(diff2), length(diff3)))
+    
+    acc[i] <- length(diff[diff])/length(diff)
+  }
+
+  
+  return(mean(acc))
+}
+
+data_x <- preds
+data_y <- outcome
+# GA parameters
+param_nBits=ncol(data_x)
+col_names=colnames(data_x)
+
+
+ga_1_knn <- ga(fitness = function(vars) calc_fitness(vars = vars, 
+                                                     data_x =  data_x, 
+                                                     data_y = data_y), # custom fitness function
+               type = "binary", # optimization data type
+               crossover=gabin_uCrossover,  # cross-over method
+               elitism = 3, # number of best ind. to pass to next iteration
+               pmutation = 0.08, # mutation rate prob
+               popSize = 50, # the number of indivduals/solutions
+               nBits = param_nBits, # total number of variables
+               names=col_names, # variable name
+               run=20, # max iter without improvement (stopping criteria)
+               maxiter = 50, # total runs or generations
+               monitor = plot, # plot the result at each iteration
+               keepBest = TRUE, # keep the best solution at the end
+               parallel = F # allow parallel procesing
+)
+
+vars_knn <- ga_1_knn@solution
+vars_knn <- vars_knn[1,]
+varNames_knn <- names(vars_knn[vars_knn == 1])
+
+data_x <- preds[,varNames_knn]
+
+
+##cross validation of accuracy
+
+y.pred_Knn <- array(NA, c(length(outcome),10,3))
+allKNN <- data.table()
+for(i in 1:10){
+  cat(".")
+  cvFolds <- createFolds(outcome,k = 5, list = F)
+  for(j in 1:5){
+    YTrain <- outcome[cvFolds != j]
+    YTest <- outcome[cvFolds == j]
+    XTrain <- data_x[cvFolds != j,]
+    XTest <- data_x[cvFolds == j,]
+    knn.fit1 <- knn(as.data.frame(XTrain), as.data.frame(XTest), YTrain, k=3)
+    knn.fit2 <- knn(as.data.frame(XTrain), as.data.frame(XTest), YTrain, k=5)
+    knn.fit3 <- knn(as.data.frame(XTrain), as.data.frame(XTest), YTrain, k=7)
+    
+
+    y.pred_Knn[cvFolds == j,i,1] <- knn.fit1
+    y.pred_Knn[cvFolds == j,i,2] <- knn.fit2
+    y.pred_Knn[cvFolds == j,i,3] <- knn.fit3
+    #diff <- pred == YTest
+    diff1 <- knn.fit1 == YTest
+    diff2 <- knn.fit2 == YTest
+    diff3 <- knn.fit3 == YTest
+    temp <- data.table(Rep = i, Fold = j, Acc_3 = length(diff1[diff1 == T])/length(diff1), Acc_5 = length(diff2[diff2 == T])/length(diff2), Acc_7 = length(diff3[diff3 == T])/length(diff3))
+    
+    allKNN <- rbind(allKNN,temp, fill = T)
+  }
+}
+knn_mean_acc <- array(NA, c(10,3))
+knn_mean_acc[,1] <- allKNN[,.(MSE = mean(Acc_3)), by = .(Rep)]$MSE
+knn_mean_acc[,2] <- allKNN[,.(MSE = mean(Acc_5)), by = .(Rep)]$MSE
+knn_mean_acc[,3] <- allKNN[,.(MSE = mean(Acc_7)), by = .(Rep)]$MSE
+best_k <- sort(colMeans(knn_mean_acc),index.return = TRUE, decreasing = TRUE)$ix[1]
+tp_knn <- array(NA, c(10,5))
+fn_knn <- array(NA, c(10,5))
+fp_knn <- array(NA, c(10,5))
+tn_knn <- array(NA, c(10,5))
+
+
+for (i in 1:10){
+  print(i)
+  temp_table <- table(y.pred_Knn[,i,best_k],outcome)
+  for (j in 1:5){
+    
+    tp_knn[i,j] <- temp_table[j,j]
+    fn_knn[i,j] <- sum(temp_table[,j])-tp_knn[i,j]
+    fp_knn[i,j] <- sum(temp_table[j,])-tp_knn[i,j]
+    tn_knn[i,j] <- sum(temp_table)-tp_knn[i,j]-fn_knn[i,j]-fp_knn[i,j]
+    
+  }
+}
+
+
+#Calculating the criteria
+miscls <- (fn_knn+fp_knn)/(tp_knn+fp_knn+tn_knn+fn_knn)
+precision <- tp_knn/(tp_knn+fp_knn)
+recall <- tp_knn/(tp_knn+fn_knn)
+f1 <- 2*(precision*recall)/(precision+recall)
+celltypenames <- array(c("AT1", "B_Cells", "Basal", "Fibroblasts", "Mast_Cells"))
+
+boxplot(f1, main = "Fi values for classes for KNN",  names =celltypenames)
+boxplot(miscls, main = "Misclassification values for classes for KNN", names  = celltypenames)
+boxplot(precision, main = "Precision values for classes for KNN", names = celltypenames)
+boxplot(recall, main = "Recall values for classes for KNN", names = celltypenames)
